0728db58f7c027bfdf8cb96e4aabcddcdfe57622
Miquel Mayordomo <miquelmayordomo@gmail.com>
2022-08-05 11:44:14 +0200

    project requirements accomplished

--- js/contact.js
+++ js/contact.js
@@ -1,53 +1,26 @@
-function postContactUs(preventForm) {
-    preventForm.preventDefault(); 
+document.getElementById("send-button").addEventListener("click", addPost)
 
+
+function addPost(preventForm) { 
+    preventForm.preventDefault();
+
+    //bring info from different inputs
     let fullName = document.getElementById("full-name").value;
     let email = document.getElementById("email").value;
     let phone = document.getElementById("phone").value;
     let message =document.getElementById("textarea").value;
-  
-    // Using fetch to push to API
+
     fetch("https://jsonplaceholder.typicode.com/posts", {
-      method: "POST",
-      headers: {
-        Accept: "text/plain, application/json, */*",
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify({ name: fullName, email: email, phone: phone, message: message}),
+        method: "POST",
+        headers: {
+            Accept: "text/plain, application/json, */*", "Content-type": "application/json",
+        },
+        body: JSON.stringify({name: fullName, email: email, phone: phone, message: message})
     })
-      .then((response) => response.json())
-      .then((contactData) => console.log(contactData));
-};
-
-const submitButton = document.getElementById("send-button");
-
-submitButton.addEventListener('click', () => {
-    postContactUs();
-});
-
-
-
-//     let fullName = document.getElementById("full-name").value;
-//     let email = document.getElementById("email").value;
-//     let phone = document.getElementById("phone").value;
-//     let message = document.getElementById("phone").value;
-
-//     const newMessage = {
-//         name: fullName,
-//         email: email,
-//         phone: phone,
-//         message: message
-//     }
-
-//     fetch("https://jsonplaceholder.typicode.com/posts", {
-//         method: "POST",
-//         body: JSON.stringify(newMessage),
-//         headers: {
-//             Accept: "text/plain, application/json, */*",
-//             "Content-type": "application/json",
-//         }
-//     })
-//     .then((response) => response.json())
-//     .then((contactData) => console.log(contactData));
-// });
-
+    .then( response => {
+        //read response status and create an alert
+        if (response.status === 201) {alert("El formulario se ha enviado correctamente")}
+        return response.json()
+      })
+      .then((formData) => console.log(formData))
+}
--- js/index.js
+++ js/index.js
@@ -14,17 +14,3 @@ function threeRecentProjects() {
     };
 
 threeRecentProjects();
-
-//calling an external API to inject one article
-function article() {
-    fetch("https://jsonplaceholder.typicode.com/posts/1")
-    .then((response) => response.json())
-    .then((res) => {
-        const article = document.getElementById("selected-project-title");
-        const content = document.getElementById("projects-text");
-        article.innerHTML = res.title.slice(0,1).toUpperCase() + res.title.slice(1,11);
-        content.innerHTML = res.body.slice(0,1).toUpperCase() + res.body.slice(1);
-    });
-}
-
-article();
